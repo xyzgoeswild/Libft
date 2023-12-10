67c8bf2e92a894d6fe5ec726159552c471d6baa8
amuhsen- <amuhsen-@c4r1p10.1337.ma>
2023-12-10 06:57:23 +0100

    Damn

---
+++ ft_itoa.c
@@ -0,0 +1,46 @@
+#include "libft.h"
+
+int	count(int num)
+{
+	unsigned int	len;
+
+	len = 1;
+	if (num == 0)
+		len++;
+	if (num < 0)
+		len++;
+	while (num != 0)
+	{
+		num = num / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	unsigned int num;
+	unsigned int len;
+	char *res;
+
+	num = n;
+	len = count(n);
+	res = (char *)malloc(len * sizeof(char));
+	if (!res)
+		return (NULL);
+	if (n == 0)
+		res[0] = '0';
+	if (n < 0)
+	{
+		res[0] = '-';
+		num = num * -1;
+	}
+	res[len - 1] = '\0';
+	while (num != 0)
+	{
+		res[len - 2] = (num % 10) + '0';
+		num = num / 10;
+		len--;
+	}
+	return (res);
+}
